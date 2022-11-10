a7193bf89cdb86fa535c47d44d880f8778eb0dbf
Josu333 <josu333@hotmail.com>
2022-11-10 22:09:12 +0100

    10/11/2022 - Parte gráfica acabada; inicio código operaciones

--- app/src/main/java/com/josue/e2mdestebanjosue/MainActivity.kt
+++ app/src/main/java/com/josue/e2mdestebanjosue/MainActivity.kt
@@ -2,10 +2,26 @@ package com.josue.e2mdestebanjosue
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.josue.e2mdestebanjosue.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private var primerNum = 0.0
+    private  var segundoNum = 0.0
+    private var operacion: String? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        operacion = null
+        //binding.btn0.setOnClickListener(binding.screen.text="0");
+
     }
+    fun pulsacion (){
+
+    }
+
 }
